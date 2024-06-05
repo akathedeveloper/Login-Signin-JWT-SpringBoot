c5c16a6def7ea892a10be0341aa755dbca737d04
Adhiraj Singh <114748865+akathedeveloper@users.noreply.github.com>
2024-06-05 14:16:52 +0530

    Initial Project

---
+++ loginSignin/src/main/java/com/loginSigninJWT/LoginSigninApplication.java
@@ -0,0 +1,13 @@
+package com.loginSigninJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSigninApplication.class, args);
+	}
+
+}
---
+++ loginSignin/src/test/java/com/loginSigninJWT/LoginSigninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginSigninJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSigninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
